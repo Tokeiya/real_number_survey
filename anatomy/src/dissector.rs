d4a9187f4e9ceeb1e24933e473b83314a5608feb
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2025-04-03 00:35:26 +0900

    wip

--- anatomy/src/dissector.rs
+++ anatomy/src/dissector.rs
@@ -1,7 +1,7 @@
 use super::mantissa::Mantissa;
 
 pub trait Dissector<const N: usize> {
-	type Target: Copy;
+	type Value: Copy;
 
 	type Exponent: Copy;
 
@@ -9,6 +9,7 @@ pub trait Dissector<const N: usize> {
 	where
 		[(); N]:;
 
+	fn value(&self) -> Self::Value;
 	fn is_nan(&self) -> bool;
 	fn is_infinite(&self) -> bool;
 	fn is_finite(&self) -> bool;
@@ -20,13 +21,26 @@ pub trait Dissector<const N: usize> {
 	fn exponent(&self) -> Self::Exponent;
 	fn mantissa(&self) -> Self::Mantissa;
 
-	fn describe_exponent(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result;
+	fn describe_exponent(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result;
 
-	fn describe_mantissa(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result;
+	fn describe_mantissa(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result;
 
-	fn describe_target(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result;
+	fn describe_target(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result;
 
-	fn describe(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+
+	// value:<round trip>
+	// sign:<+/->
+	// mantissa:<binary format and hex>
+	// exponent:<integer with sign>
+	// is_nan:bool
+	// is_inf:bool
+	// is_nomal:bool
+
+	fn describe(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f,"value:")?;
+		Self::describe_target(self, f)?;
+		writeln!(f,"")?;
+		
 		todo!()
 	}
 }
